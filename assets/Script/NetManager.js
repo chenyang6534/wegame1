6a88296105aa2c924907212e28a853c484a33306
chenyang <357328436@qq.com>
2018-04-04 17:00:04 +0800

    微信wss

--- assets/Script/NetManager.js
+++ assets/Script/NetManager.js
@@ -6,8 +6,10 @@
 
 
 
-var serverSrc = "ws://127.0.0.1:1117/connect"
-
+//var serverSrc = "ws://127.0.0.1:1117/connect"
+//var serverSrc = "ws://www.game5868.top/connect1"
+var serverSrc = "wss://www.game5868.top/connect"
+var wxServerSrc = "wss://www.game5868.top/connect"
 var Msg = require("Msg")
 var MsgManager = require("MsgManager")
 
@@ -31,8 +33,8 @@ var NetManager = cc.Class({
         this.loginFail = failCallBack
 
 
-        this.QuickLogin("ios","123456789")
-        //this.WXLogin()
+        //this.QuickLogin("ios","123456789")
+        this.WXLogin()
     },
 
     WXLogin:function(){
@@ -52,20 +54,35 @@ var NetManager = cc.Class({
                     console.log(res2.userInfo.avatarUrl);
                     //请求自己的服务器
                     //Login(code,encryptedData,iv);
-                    this.m_webSocket = new WebSocket(serverSrc);
-                    this.m_webSocket.onopen = function (event) {
-                        this.m_webSocket.send(Msg.CS_MsgWeiXingLogin(code,res2.userInfo.nickName))
-                    }.bind(this);
-                    this.m_webSocket.onmessage = function (event) {
-                        console.log("response text msg: " + event.data);
-                        MsgManager.getInstance().ParseMsg(event.data)
-                    }.bind(this);
-                    this.m_webSocket.onerror = function (event) {
-                        console.log("Send Text fired an error");
-                    }.bind(this);
-                    this.m_webSocket.onclose = function (event) {
-                        console.log("WebSocket instance closed.");
-                    }.bind(this);
+                    
+                    this.m_webSocket = wx.connectSocket({
+                        url: wxServerSrc
+                    })
+                    wx.onSocketOpen(function (res) {
+                        console.log("onSocketOpen")
+                        console.log(Msg.CS_MsgWeiXingLogin(code,res2.userInfo.nickName))
+
+                        // wx.sendSocketMessage({
+                        //     data:Msg.CS_MsgWeiXingLogin(code,res2.userInfo.nickName)
+                        // })
+
+                        this.m_webSocket.send({
+                                data:Msg.CS_MsgWeiXingLogin(code,res2.userInfo.nickName)
+                            })
+                    }.bind(this))
+                    wx.onSocketError(function (res) {
+                        console.log('WebSocket 连接打开失败，请检查！')
+                    }.bind(this))
+                    wx.onSocketClose(function (res) {
+                        console.log('WebSocket 连接关闭！')
+                    }.bind(this))
+                    wx.onSocketMessage(function (res) {
+                        console.log('WebSocket 收到消息')
+                        console.log(res)
+                        MsgManager.getInstance().ParseMsg(res.data)
+                    }.bind(this))
+                    
+
                   }
                 })
           
@@ -78,7 +95,10 @@ var NetManager = cc.Class({
 
     QuickLogin:function(Platform,MachineId){
         this.m_webSocket = new WebSocket(serverSrc);
+        console.log(this.m_webSocket)
+        console.log("1245")
         this.m_webSocket.onopen = function (event) {
+            console.log("onopen")
             this.m_webSocket.send(Msg.CS_MsgQuickLogin(Platform,MachineId))
         }.bind(this);
         this.m_webSocket.onmessage = function (event) {
